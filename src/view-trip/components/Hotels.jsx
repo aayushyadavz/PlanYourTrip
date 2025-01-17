f3be8bf7c2851b68d80ea25dd268fe5d619c528e
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-01-17 20:42:18 +0530

    refactor Hotels and PlacesToVisit components to use updated data structure for hotel options and itinerary

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -6,15 +6,15 @@ const Hotels = ({ trip }) => {
             <h2 className="font-bold text-xl mt-5">Hotel Recommendations</h2>
 
             <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {trip?.aiResponse?.travel_plan?.hotel_options.map((hotel, index) => (
-                    <Link to={'https://www.google.com/maps/search/?api=1&query=' + hotel?.name + ',' + hotel?.address} key={index} target="_blank">
+                {trip?.aiResponse?.hotels.map((hotel, index) => (
+                    <Link to={'https://www.google.com/maps/search/?api=1&query=' + hotel.name + ',' + hotel.address} key={index} target="_blank">
                         <div className="hover:scale-105 transition-all cursor-pointer">
                             <img src="https://www.rosenaviation.com/wp-content/uploads/2024/02/Longest-commercial-flights-Rosen-Aviation-scaled.jpeg" className="rounded-xl" />
                             <div className="my-2 flex flex-col gap-2">
-                                <h2 className="font-medium">{hotel?.name}</h2>
-                                <h2 className="text-xs text-gray-500">📍 {hotel?.address}</h2>
-                                <h2 className="text-sm">💰 {hotel?.price_range}</h2>
-                                <h2 className="text-sm">⭐ {hotel?.rating}</h2>
+                                <h2 className="font-medium">{hotel.name}</h2>
+                                <h2 className="text-xs text-gray-500">📍 {hotel.address}</h2>
+                                <h2 className="text-sm">💰 {hotel.price_range}</h2>
+                                <h2 className="text-sm">⭐ {hotel.rating}</h2>
                             </div>
                         </div>
                     </Link>
--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -7,7 +7,7 @@ const PlacesToVisit = ({ trip }) => {
             <h2 className='font-bold text-lg'>Places to Visit</h2>
 
             <div>
-                {trip?.aiResponse?.travel_plan?.itinerary.map((item, index) => (
+                {trip?.aiResponse?.itinerary.map((item, index) => (
                     <div key={index} className='mt-5'>
                         <h2 className='font-medium text-lg'>Day {item.day}</h2>
                         <div className='grid md:grid-cols-2 gap-5'>
