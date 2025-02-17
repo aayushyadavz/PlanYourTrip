cde71009466cf6a531310634f7545b4337b238f1
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-02-17 11:54:00 +0530

    refactor: update AI_PROMPT for clarity and enhance UserTripCard styling

--- src/my-trips/components/UserTripCard.jsx
+++ src/my-trips/components/UserTripCard.jsx
@@ -21,12 +21,12 @@ const UserTripCard = ({ trip }) => {
 
     return (
         <Link to={'/view-trip/' + trip.id}>
-            <div className="border p-2 h-[210px] sm:h-[240px] rounded-xl hover:shadow-lg hover:scale-105 transition-all cursor-pointer relative">
+            <div className="border p-2 h-[210px] sm:h-[240px] rounded-xl shadow-lg cursor-pointer relative">
                 <img src={photoUrl} alt="" className="rounded-xl object-cover h-28 sm:h-36 w-full" />
                 <div
-                    className="absolute top-3 right-3 bg-black bg-opacity-20 rounded-full p-2"
+                    className="absolute top-3 right-3 bg-white rounded-full p-2 hover:shadow-lg hover:scale-110 transition-all"
                     onClick={handleDelete}
-                ><RiDeleteBin6Fill className="text-white text-lg" /></div>
+                ><RiDeleteBin6Fill className="text-[#f56551] text-lg" /></div>
                 <div>
                     <h2 className="font-bold text-lg text-black line-clamp-1 my-1">
                         {trip.userSelection.location.label}
